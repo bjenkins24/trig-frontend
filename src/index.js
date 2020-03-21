630dc42bf51448019312d9761a17bdd2543fad3f
Brian Jenkins <bjenkins@yourmusiclessons.com>
2020-03-21 06:31:45 -0700

    Fix prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const Page = () => {
   return <div>My test site</div>;
 };
 
-ReactDOM.render(<Page />, document.getElementById("app"));
+ReactDOM.render(<Page />, document.getElementById('app'));

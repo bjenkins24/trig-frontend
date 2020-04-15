8186692e85d507df5c87eadad45544bec015ec3e
Brian Jenkins <bjenkins@yourmusiclessons.com>
2020-04-15 13:42:31 -0700

    Card view with href

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -40,103 +40,116 @@ const CardView = () => {
       </ButtonToggle>
       {cards && view === 'thumbnail' && (
         <StackGrid columnWidth={251} gutterWidth={16} gutterHeight={16}>
-          {cards.map(({ id, title, actual_created_at: createdAt }) => {
-            return (
-              <Card
-                onClick={() => null}
-                key={id}
-                title={title}
-                dateTime={new Date(createdAt)}
-                type="youtube"
-                renderAvatar={() => {
-                  return <Avatar size={1.6} />;
-                }}
-                totalFavorites={0}
-                isFavorited={false}
-                totalComments={0}
-                onClickFavorite={() => null}
-                onClickComment={() => null}
-                navigationList={[
-                  {
-                    onClick: () => null,
-                    item: (
-                      <HorizontalGroup margin={1.6}>
-                        <Icon type="new-window" size={1.6} />
-                        <span>Open in New Window</span>
-                      </HorizontalGroup>
-                    ),
-                  },
-                  {
-                    onClick: () => null,
-                    item: (
-                      <HorizontalGroup margin={1.6}>
-                        <Icon type="edit" size={1.6} />
-                        <span>Edit Card</span>
-                      </HorizontalGroup>
-                    ),
-                  },
-                  {
-                    onClick: () => null,
-                    item: (
-                      <HorizontalGroup margin={1.6}>
-                        <Icon type="lock" size={1.6} />
-                        <span>Share</span>
-                      </HorizontalGroup>
-                    ),
-                  },
-                  {
-                    onClick: () => null,
-                    item: (
-                      <HorizontalGroup margin={1.6}>
-                        <Icon type="trash" size={1.6} />
-                        <span>Remove From Trig</span>
-                      </HorizontalGroup>
-                    ),
-                  },
-                ]}
-              />
-            );
-          })}
+          {cards.map(
+            ({
+              id,
+              title,
+              actual_created_at: createdAt,
+              card_link: { link },
+            }) => {
+              return (
+                <Card
+                  onClick={() => {
+                    window.location.href = link;
+                  }}
+                  href={link}
+                  key={id}
+                  title={title}
+                  dateTime={new Date(createdAt)}
+                  type="youtube"
+                  renderAvatar={() => {
+                    return <Avatar size={1.6} />;
+                  }}
+                  totalFavorites={0}
+                  isFavorited={false}
+                  totalComments={0}
+                  onClickFavorite={() => null}
+                  onClickComment={() => null}
+                  navigationList={[
+                    {
+                      onClick: () => null,
+                      item: (
+                        <HorizontalGroup margin={1.6}>
+                          <Icon type="new-window" size={1.6} />
+                          <span>Open in New Window</span>
+                        </HorizontalGroup>
+                      ),
+                    },
+                    {
+                      onClick: () => null,
+                      item: (
+                        <HorizontalGroup margin={1.6}>
+                          <Icon type="edit" size={1.6} />
+                          <span>Edit Card</span>
+                        </HorizontalGroup>
+                      ),
+                    },
+                    {
+                      onClick: () => null,
+                      item: (
+                        <HorizontalGroup margin={1.6}>
+                          <Icon type="lock" size={1.6} />
+                          <span>Share</span>
+                        </HorizontalGroup>
+                      ),
+                    },
+                    {
+                      onClick: () => null,
+                      item: (
+                        <HorizontalGroup margin={1.6}>
+                          <Icon type="trash" size={1.6} />
+                          <span>Remove From Trig</span>
+                        </HorizontalGroup>
+                      ),
+                    },
+                  ]}
+                />
+              );
+            }
+          )}
         </StackGrid>
       )}
 
       {cards && view === 'row' && (
         <List>
-          {cards.map(({ title, actual_created_at: createdAt }) => {
-            return (
-              <ListItem
-                onClick={() => null}
-                renderItem={() => <FileIcon type="doc" size={2.4} />}
-                renderContent={() => (
-                  <ListItemContent
-                    renderItem={() => <Avatar size={4} />}
-                    primary={title}
-                    secondary={format(new Date(createdAt), 'MMM d, yyyy')}
-                  />
-                )}
-                actions={[
-                  <Icon
-                    onClick={() => null}
-                    type="heart"
-                    color="s"
-                    size={1.6}
-                  />,
-                  <Icon
-                    type="comment"
-                    color="s"
-                    size={1.6}
-                    onClick={() => null}
-                  />,
-                  <Icon
-                    type="horizontal-dots"
-                    color="s"
-                    size={1.6}
-                    onClick={() => null}
-                  />,
-                ]}
-              />
-            );
-          })}
+          {cards.map(
+            ({ title, actual_created_at: createdAt, card_link: { link } }) => {
+              return (
+                <ListItem
+                  href={link}
+                  onClick={() => null}
+                  renderItem={() => <FileIcon type="doc" size={2.4} />}
+                  renderContent={() => (
+                    <ListItemContent
+                      renderItem={() => <Avatar size={4} />}
+                      primary={title}
+                      secondary={format(new Date(createdAt), 'MMM d, yyyy')}
+                    />
+                  )}
+                  actions={[
+                    <Icon
+                      onClick={() => null}
+                      type="heart"
+                      color="s"
+                      size={1.6}
+                    />,
+                    <Icon
+                      type="comment"
+                      color="s"
+                      size={1.6}
+                      onClick={() => null}
+                    />,
+                    <Icon
+                      type="horizontal-dots"
+                      color="s"
+                      size={1.6}
+                      onClick={() => null}
+                    />,
+                  ]}
+                />
+              );
+            }
+          )}
         </List>
       )}
     </div>

361a1c649b5f7cd34fd8dfd4a6f2b5c54c397ca7
Brian Jenkins <brian@trytrig.com>
2020-09-27 11:16:53 -0700

    Scroll to top on page chnage

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Switch, Route, useHistory } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Switch, Route, useHistory, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Header from './components/Header';
@@ -10,6 +10,11 @@ import useSearch from './utils/useSearch';
 const AuthenticatedApp = () => {
   const history = useHistory();
   const { isSearchOpen, openSearch, closeSearch, searchKey } = useSearch();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return (
     <div>

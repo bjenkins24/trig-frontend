312be19e623ecf8dc147db12e1542d5c420d3f55
Brian Jenkins <brian@trytrig.com>
2021-04-21 14:11:18 -0700

    Add sentry and chrome extension url

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { ToastContainer } from '@trig-app/core-components';
@@ -26,6 +27,8 @@ if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn:
       'https://f0ddc4ee203d4ecc8eec64ab1e377c8c@o422035.ingest.sentry.io/5391333',
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
   });
 }
 

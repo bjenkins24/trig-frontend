c9633b0ab39ce37f90e1d3ab6d63a5fc4b7bd0d7
Brian Jenkins <brian@trytrig.com>
2021-04-29 14:36:45 -0700

    Add register to authenticated route as well

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import useUser from './utils/useUser';
 import AuthenticatedApp from './AuthenticatedApp';
 import UnauthenticatedApp from './UnauthenticatedApp';
 import './utils/consoleOverrides';
+import Register from './pages/Register';
 
 const App = () => {
   const { user, isLoading } = useUser();
 
   if (isLoading) return null;
 
-  return user ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+  return user ? (
+    <>
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route>
+          <AuthenticatedApp />
+        </Route>
+      </Switch>
+    </>
+  ) : (
+    <UnauthenticatedApp />
+  );
 };
 
 export default App;

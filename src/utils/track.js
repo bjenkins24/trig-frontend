923ba19d5a1573b0fd31689980da9b0db9d698c5
Brian Jenkins <brian@trytrig.com>
2021-06-16 15:17:09 -0700

    Change segment tracking depending on env

--- src/utils/track.js
+++ src/utils/track.js
@@ -1,3 +1,15 @@
+import Analytics from 'analytics';
+import segmentPlugin from '@analytics/segment';
+
+const analytics = Analytics({
+  app: 'awesome-app',
+  plugins: [
+    segmentPlugin({
+      writeKey: process.env.SEGMENT_WRITE_KEY,
+    }),
+  ],
+});
+
 export const identify = user => {
   analytics.identify(user.id, {
     email: user.email,
